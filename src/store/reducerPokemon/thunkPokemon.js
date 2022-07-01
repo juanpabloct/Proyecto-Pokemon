e84695157a6e068a4c0f981038948a2ce72e3edb
Juanpabloct <juanptorres2003@gmail.com>
2022-07-01 08:51:33 -0500

    se esta usando fetch para pedir datos

--- src/store/reducerPokemon/thunkPokemon.js
+++ src/store/reducerPokemon/thunkPokemon.js
@@ -4,7 +4,6 @@ export const pokemon=(page=0)=>{
     return  async (dispatch, getState)=>{
         dispatch(isLoading())
         const allPokemons=await (await fetch(`https://pokeapi.co/api/v2/pokemon?limit=11&offset=${page*11}`)).json()
-
         return dispatch(setPokemons({pokemons:allPokemons.results, page}))
     }
 }
